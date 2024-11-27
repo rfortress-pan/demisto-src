951b086c71f64e88a58d16d695510901b63baa1b
rfortress-pan <107966748+rfortress-pan@users.noreply.github.com>
2024-11-27 12:56:12 -0700

    Update xsoar_dev.sh

--- xsoar_dev.sh
+++ xsoar_dev.sh
@@ -2,12 +2,23 @@
 
 clear
 
+# Check to see which version of python is installed
 if command -v python3 >/dev/null 2>&1; then
     PY="python3"
 elif command -v python >/dev/null 2>&1; then
     PY="python"
 else
     echo "${RED}!!{$END} Neither python nor python3 is installed." >&2
+    exit 1
+fi
+
+# Check to see which version of pip is installed
+if command -v pip3 >/dev/null 2>&1; then
+    PIP="pip3"
+elif command -v pip >/dev/null 2>&1; then
+    PIP="pip"
+else
+    echo "${RED}!!{$END} PIP is not installed. Try running ${GREEEN}pip${END} or ${GREEN}pip3${END} and it should give instructions on how to install it." >&2
     exit 1
 fi
 
